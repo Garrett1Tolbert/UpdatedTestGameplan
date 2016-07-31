2b69f65920d1996d3ed51d222685af3b1f0f27f9
Garrett1Tolbert <garrett1.tolbert@gmail.com>
2016-07-31 13:46:54 -0400

    Update Tasks.java

--- app/src/main/java/com/example/garrett/updatedtestgameplan/Tasks.java
+++ app/src/main/java/com/example/garrett/updatedtestgameplan/Tasks.java
@@ -2,7 +2,15 @@ package com.example.garrett.updatedtestgameplan;
 
 
 public class Tasks {
+    int id;
     String task;
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
 
     public void setTask(String task){
         this.task = task;
